896e77941e3307f29b3d88aacb7b38949bb1afd9
Cory Johns <johnsca@gmail.com>
2019-03-01 09:30:53 +0100

    Initial stab at a k8s charm template

---
+++ reactive/k8s_charm_template.py
@@ -0,0 +1,71 @@
+from charmhelpers.core import hookenv
+from charms.reactive import set_flag, clear_flag
+from charms.reactive import when_all, when_any, when_not
+
+from charms import layer
+
+
+@when_all('layer.docker-resource.my-image.available',
+          'layer.docker-resource.my-other-image.available')
+@when_not('charm.k8s-charm-template.started')
+def start_charm():
+    layer.status.maintenance('configuring workload')
+
+    # fetch the info (registry path, auth info) about the images
+    image1_info = layer.docker_resource.get_info('my-image')
+    image2_info = layer.docker_resource.get_info('my-other-image')
+
+    # this can also be handed raw YAML, so some charm authors
+    # choose to use templated YAML in a file instead
+    layer.caas_base.pod_spec_set({
+        'containers': [
+            {
+                'name': 'k8s-charm-template-service1',
+                'imageDetails': {
+                    'imagePath': image1_info.registry_path,
+                    'username': image1_info.username,
+                    'password': image1_info.password,
+                },
+                'command': [],
+                'ports': [
+                    {
+                        'name': 'website1',
+                        'containerPort': 80,
+                    },
+                ],
+                'config': {
+                    'MY_ENV_VAR': hookenv.model_name(),
+                },
+            },
+            {
+                'name': 'k8s-charm-template-service2',
+                'imageDetails': {
+                    'imagePath': image2_info.registry_path,
+                    'username': image2_info.username,
+                    'password': image2_info.password,
+                },
+                'command': [],
+                'ports': [
+                    {
+                        'name': 'website2',
+                        'containerPort': 8080,
+                    },
+                ],
+                'config': {
+                    'MY_ENV_VAR': hookenv.model_name(),
+                },
+            },
+        ],
+    })
+
+    # juju should set the status to active once the workload
+    # pod is up and running (I think?)
+    layer.status.maintenance('starting workload')
+    set_flag('charm.k8s-charm-template.started')
+
+
+@when_any('layer.docker-resource.my-image.changed',
+          'layer.docker-resource.my-other-image.changed')
+def update_image():
+    # handle a new image resource becoming available
+    clear_flag('charm.k8s-charm-template.started')
